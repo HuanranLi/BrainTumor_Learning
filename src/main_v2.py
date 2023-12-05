eed64133db10fa9d14d7198d268bc7488be76a53
Huanran Li <nemo0421@gmail.com>
2023-12-05 13:37:46 -0800

    Update main_v2.py

--- src/main_v2.py
+++ src/main_v2.py
@@ -47,7 +47,7 @@ def CL_evaluate_model(model, data_loader, loss_function, device):
 train_hyperparams = {
     'batch_size': 128,
     'learning_rate': 0.001,
-    'num_epochs': 5,
+    'num_epochs': 200,
     'resize': (224, 224),
     'normalize_means': (0.5, 0.5, 0.5),
     'normalize_stds': (0.5, 0.5, 0.5),
@@ -143,7 +143,7 @@ def FT_evaluate_model(data_loader, CL_model, FT_model, device, criterion):
 tune_hyperparams = {
     'batch_size': 32,
     'learning_rate': 0.001,
-    'num_epochs': 5,
+    'num_epochs': 200,
     'resize': (224, 224),
     'normalize_means': (0.5, 0.5, 0.5),
     'normalize_stds': (0.5, 0.5, 0.5),
