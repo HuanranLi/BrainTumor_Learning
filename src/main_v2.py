dc8c70033e1d750a58c98a7cbbefe92db4f034a8
Huanran Li <nemo0421@gmail.com>
2023-12-08 11:08:43 -0800

    Update main_v2.py

--- src/main_v2.py
+++ src/main_v2.py
@@ -156,7 +156,7 @@ tune_hyperparams = {
     'test_dataset_dir': '../dataset/brain_tumor/Testing',
 }
 FT = {}
-FT['train_loader'], FT['test_loader'] = load_data(tune_hyperparams, CL = False, sample_rate = 0.5)
+FT['train_loader'], FT['test_loader'] = load_data(tune_hyperparams, CL = False, sample_rate = 0.3)
 FT['model'], FT['device'] = setup_model(tune_hyperparams)
 FT['optimizer'], FT['loss_function'] = init_optimizer_loss(tune_hyperparams, FT['model'])
 
